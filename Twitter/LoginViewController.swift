aec236999cbbf71cd1979a41bfca8535de455c38
Daniel St. Vincent <59455549+danieljstvincent@users.noreply.github.com>
2020-10-16 23:41:10 -0700

    📝 `NOTE` Paste this template at the top of your existing `README.md` file from part 1 of this assignment. (🚫 Remove this paragraph after after checking off completed user stories)
    # Twitter - Part II
    
    This is a basic twitter app to view, compose, favorite, and retweet tweets.
    
    Time spent: **20** hours spent in total
    
    ## User Stories
    
    The following **required** functionality is completed:
    
    - [ ] User can compose a tweet. (4pts)
    - [ ] User can favorite a tweet. (4pts)
    - [ ] User can retweet a tweet. (2pts)
    
    The following **bonus** features are implemented:
    
    - [ ] When composing a tweet, user sees a countdown for the number of characters remaining for the tweet (out of 280) (2pts)
    - [ ] User can view their profile in a profile tab. (3pts)
    - [ ] User sees embedded images in tweet if available. (3pts)
    
    ## Video Walkthrough
    
    Here's a walkthrough of implemented user stories:
    
    <img src='http://g.recordit.co/XPbKLAsJ7f.gif'' alt='Video Walkthrough' />

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Daniel St Vincent on 10/16/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: IUViewController {
+
+}
